3cc24d2d7dcc040f4730ae032c28137645efdb03
olivadariojose <olivadariojose@gmail.com>
2025-09-23 19:46:32 -0600

    tarea 3 completada

--- server.js
+++ server.js
@@ -1,45 +1,13 @@
-
-// // server.js
-// 'use strict';
-// require('dotenv').config();
-// const express = require('express');
-// const myDB = require('./connection');
-// const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
-// const app = express();
-
-// // CORS abierto para el tester de freeCodeCamp
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//   next();
-// });
-
-// fccTesting(app); // For FCC testing purposes
-
-// app.set('view engine', 'pug');
-// app.set('views', './views/pug');
-
-// app.use('/public', express.static(process.cwd() + '/public'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.get('/', (req, res) => {
-//   res.render('index'); // renderiza views/pug/index.pug
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log('Listening on port ' + PORT);
-// });
-
-
 // server.js
 'use strict';
 require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+// nuevos requires
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -58,6 +26,16 @@ app.set('views', './views/pug');
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// session + passport
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'mysecret',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.render('index', {
