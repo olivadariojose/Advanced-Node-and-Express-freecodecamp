03b0f305f59925057876220cbf93c7f05533abeb
olivadariojose <olivadariojose@gmail.com>
2025-09-23 19:34:04 -0600

    ejercicio 2 completado

--- server.js
+++ server.js
@@ -1,3 +1,40 @@
+
+// // server.js
+// 'use strict';
+// require('dotenv').config();
+// const express = require('express');
+// const myDB = require('./connection');
+// const fccTesting = require('./freeCodeCamp/fcctesting.js');
+
+// const app = express();
+
+// // CORS abierto para el tester de freeCodeCamp
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
+
+// fccTesting(app); // For FCC testing purposes
+
+// app.set('view engine', 'pug');
+// app.set('views', './views/pug');
+
+// app.use('/public', express.static(process.cwd() + '/public'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// app.get('/', (req, res) => {
+//   res.render('index'); // renderiza views/pug/index.pug
+// });
+
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log('Listening on port ' + PORT);
+// });
+
+
+// server.js
 'use strict';
 require('dotenv').config();
 const express = require('express');
@@ -6,13 +43,30 @@ const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
+// CORS para el tester de freeCodeCamp
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+fccTesting(app);
+
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Hello',
+    message: 'Please log in',
+    showLogin: true,
+    showRegistration: true,
+    showSocialAuth: true
+  });
 });
 
 const PORT = process.env.PORT || 3000;
